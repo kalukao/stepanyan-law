959a657059584c9312a78da48ae4a6444a581e05
kalukao <47899981+kalukao@users.noreply.github.com>
2025-02-26 12:02:03 +0100

    Create Footer.jsx

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white py-6">
+      <div className="container mx-auto px-4 text-center">
+        <p>© 2025 Law Office of Izabella Stepanyan. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
